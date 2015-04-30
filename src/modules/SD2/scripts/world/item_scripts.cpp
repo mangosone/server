f7cd464d3152994af6d47b47c3367a24ee980b16
SylvainNeau <syneau@gmail.com>
2015-04-30 08:44:15 +0200

    Ogre brew only in Blade's Edge
    The blue and red ogre brew couldn't be used elsewhere than blade's edge
    plateaus

--- src/modules/SD2/scripts/world/item_scripts.cpp
+++ src/modules/SD2/scripts/world/item_scripts.cpp
@@ -37,11 +37,51 @@
  * item_arcane_charges                 Prevent use if player is not flying (cannot cast while on ground)
  * item_flying_machine(i34060,i34061)  Engineering crafted flying machines
  * item_gor_dreks_ointment(i30175)     Protecting Our Own(q10488)
+ * item_ogre_brew					   Prevent use if player not in 3522 area
  * EndContentData
  */
 
 #include "precompiled.h"
 #include "Spell.h"
+
+/*#####
+# item_ogre_brew
+#####*/
+
+enum
+{
+    BLUE_OGRE_BREW	= 32783,
+	RED_OGRE_BREW	= 32784,
+	TRANCHANTES		= 3522,
+	OGRE_BREW_MANA	= 41304,
+	OGRE_BREW_LIFE	= 41306
+};
+
+bool ItemUse_item_ogre_brew(Player* pPlayer, Item* pItem, const SpellCastTargets&)
+{
+	uint32 itemId	= pItem->GetEntry();
+	uint32 pZoneId	= pPlayer->GetZoneId();
+	
+	if (itemId == BLUE_OGRE_BREW || itemId == RED_OGRE_BREW)
+	{
+		if (pZoneId == TRANCHANTES)
+		{
+			return false;
+		}
+		debug_log("SD2: Player attempt to use item %u, but did not meet zone requirement : %u", itemId,pZoneId);
+		if (itemId == BLUE_OGRE_BREW)
+		{
+			const SpellEntry* pSpellInfo = GetSpellStore()->LookupEntry(OGRE_BREW_LIFE);
+			Spell::SendCastResult(pPlayer, pSpellInfo, 1, SPELL_FAILED_NOT_HERE);
+		}
+		if (itemId == RED_OGRE_BREW)
+		{
+			const SpellEntry* pSpellInfo = GetSpellStore()->LookupEntry(OGRE_BREW_MANA);
+			Spell::SendCastResult(pPlayer, pSpellInfo, 1, SPELL_FAILED_NOT_HERE);
+		}
+		return true;
+	}
+}
 
 /*#####
 # item_arcane_charges
@@ -114,6 +154,11 @@ bool ItemUse_item_gor_dreks_ointment(Player* pPlayer, Item* pItem, const SpellCa
 void AddSC_item_scripts()
 {
     Script* pNewScript;
+
+	pNewScript = new Script;
+    pNewScript->Name = "item_ogre_brew";
+    pNewScript->pItemUse = &ItemUse_item_ogre_brew;
+    pNewScript->RegisterSelf();
 
     pNewScript = new Script;
     pNewScript->Name = "item_arcane_charges";
