db766933ba125a246c1446633da625c59b4f9b0f
Antz <billy1arm@users.noreply.github.com>
2015-09-13 08:59:00 +0100

    [DB] Updated Expected DB version to Rel21.3.0

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -37,7 +37,7 @@
     #define CHAR_DB_UPDATE_DESCRIPTION "revision_refactor"
 
     #define WORLD_DB_VERSION_NR 21
-    #define WORLD_DB_STRUCTURE_NR 1
+    #define WORLD_DB_STRUCTURE_NR 3
     #define WORLD_DB_CONTENT_NR 0
-    #define WORLD_DB_UPDATE_DESCRIPTION "revision_refactor"
+    #define WORLD_DB_UPDATE_DESCRIPTION "warden_multiversion"
 #endif // __REVISION_H__
