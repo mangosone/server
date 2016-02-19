a78cb434862f4d18de3caab13ea56c3b4e6cb149
Olion17 <oleg70ua@yahoo.com>
2016-02-19 09:57:30 +0000

    Not so bright warning if DB content newer than core awaits
    [Core] Add negative aura exception for spell 37695 [cs2317]

--- src/game/Object/SpellMgr.cpp
+++ src/game/Object/SpellMgr.cpp
@@ -727,6 +727,7 @@ bool IsPositiveEffect(SpellEntry const* spellproto, SpellEffectIndex effIndex)
                         case 18172:                         // Quest Kodo Roundup player debuff
                         case 23445:                         // evil twin
                         case 35679:                         // Protectorate Demolitionist
+                        case 37695:                         // Stanky
                         case 38637:                         // Nether Exhaustion (red)
                         case 38638:                         // Nether Exhaustion (green)
                         case 38639:                         // Nether Exhaustion (blue)
