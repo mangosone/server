ceb04105abf60be80f83e12389fd8b7dda37cc2f
H0zen <H0zen@users.noreply.github.com>
2016-09-18 18:40:46 +0300

    Fix server crash when NPC pets die due to periodic damage auras.
    Note: does not apply to Zero!

--- src/game/Object/Pet.cpp
+++ src/game/Object/Pet.cpp
@@ -576,8 +576,8 @@ void Pet::Update(uint32 update_diff, uint32 diff)
             {
                 MANGOS_ASSERT(getPetType() != SUMMON_PET);  // Pet must be already removed
                 Unsummon(PET_SAVE_NOT_IN_SLOT);             // hunters' pets never get removed because of death, NEVER!
-                break;
             }
+            break;
         }
         case ALIVE:
         {
