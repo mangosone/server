aa0ff1854454684dce855500b9c8dbdd1573a649
Antz <antz@getmangos.eu>
2017-05-31 09:27:39 +0100

    [DB] Update base world DB to Rel21.7.18

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -24,7 +24,7 @@
 
 #ifndef MANGOS_H_REVISION
 #define MANGOS_H_REVISION
-    #define REVISION_NR "2107006"
+    #define REVISION_NR "2107018"
 
     #define REALMD_DB_VERSION_NR 21
     #define REALMD_DB_STRUCTURE_NR 1
@@ -38,6 +38,6 @@
 
     #define WORLD_DB_VERSION_NR 21
     #define WORLD_DB_STRUCTURE_NR 7
-    #define WORLD_DB_CONTENT_NR 6
-    #define WORLD_DB_UPDATE_DESCRIPTION "Gossip_texts_updates"
+    #define WORLD_DB_CONTENT_NR 18
+    #define WORLD_DB_UPDATE_DESCRIPTION "Silvermyst_Isle_correction"
 #endif // __REVISION_H__
