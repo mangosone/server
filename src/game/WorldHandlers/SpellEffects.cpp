21f22cdb6afe5e1d0530cb13e5b77db3abbfc8c6
JapanLady42 <akemi.nakamura42@gmail.com>
2017-07-06 17:43:19 -0700

    Hackfix to make devastate cast Sunder Armor

--- src/game/WorldHandlers/SpellEffects.cpp
+++ src/game/WorldHandlers/SpellEffects.cpp
@@ -4678,6 +4678,8 @@ void Spell::EffectWeaponDmg(SpellEffectIndex eff_idx)
                     // 25 * stack
                     unitTarget->AddThreat(m_caster, 25.0f * sunder->GetStackAmount(), false, GetSpellSchoolMask(m_spellInfo), m_spellInfo);
                 }
+                else
+					m_caster->CastSpell(unitTarget, 11597, true);
             }
             break;
         }
