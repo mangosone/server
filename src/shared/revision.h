8c116b06cde482b43216c46fbd386a56de075257
Antz <antz@getmangos.eu>
2018-01-07 23:04:14 +0000

    Revert "Remove Remnants of Two obsolete tables: npc_trainer_template & npc_vendor_template"
    This reverts commit 4a633be8b327af8d7a7102b542d7b90b56281887.
    
    NOTE: This goes to prove that your can never trust the code or database
          comments to be 100% true

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -28,16 +28,16 @@
 
     #define REALMD_DB_VERSION_NR 21
     #define REALMD_DB_STRUCTURE_NR 1
-    #define REALMD_DB_CONTENT_NR 3
-    #define REALMD_DB_UPDATE_DESCRIPTION "Remove field from dbDocs"
+    #define REALMD_DB_CONTENT_NR 4
+    #define REALMD_DB_UPDATE_DESCRIPTION "Remove dbDocs"
 
     #define CHAR_DB_VERSION_NR 21
     #define CHAR_DB_STRUCTURE_NR 3
-    #define CHAR_DB_CONTENT_NR 1
-    #define CHAR_DB_UPDATE_DESCRIPTION "characters_pvpstats"
+    #define CHAR_DB_CONTENT_NR 2
+    #define CHAR_DB_UPDATE_DESCRIPTION "remove_dbdocs"
 
     #define WORLD_DB_VERSION_NR 21
     #define WORLD_DB_STRUCTURE_NR 8
-    #define WORLD_DB_CONTENT_NR 12
-    #define WORLD_DB_UPDATE_DESCRIPTION "Commands_update"
+    #define WORLD_DB_CONTENT_NR 23
+    #define WORLD_DB_UPDATE_DESCRIPTION "revert_remove_templates"
 #endif // __REVISION_H__
