911b6a8b4de094fbd2f3c91665e4289dbd9dd84d
Necrovoice <necrovoice@getmangos.eu>
2020-05-11 16:53:07 -0400

    Revision update
    Needed to support table update to remove unique Index.

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -37,7 +37,7 @@
     #define CHAR_DB_UPDATE_DESCRIPTION "Add_Field_Comments"
 
     #define WORLD_DB_VERSION_NR 21
-    #define WORLD_DB_STRUCTURE_NR 12
+    #define WORLD_DB_STRUCTURE_NR 13
     #define WORLD_DB_CONTENT_NR 001
-    #define WORLD_DB_UPDATE_DESCRIPTION "warden_refactor"
+    #define WORLD_DB_UPDATE_DESCRIPTION "UBRS_equip_models_add"
 #endif // __REVISION_H__
