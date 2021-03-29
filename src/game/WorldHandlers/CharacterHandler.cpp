f852e5ffae43893602b2742fa683b9b23c78bde6
Antz <billy1arm@users.noreply.github.com>
2021-03-29 07:45:35 +0100

    [Bots] Fix bots teleporting and starting in Outland areas' Thanks Henri Defoux

--- src/game/WorldHandlers/CharacterHandler.cpp
+++ src/game/WorldHandlers/CharacterHandler.cpp
@@ -197,7 +197,7 @@ class CharacterHandler
             // The bot's WorldSession is owned by the bot's Player object
             // The bot's WorldSession is deleted by PlayerbotMgr::LogoutPlayerBot
             uint32 botAccountId = lqh->GetAccountId();
-            WorldSession *botSession = new WorldSession(botAccountId, NULL, SEC_PLAYER, 0,0, LOCALE_enUS);
+            WorldSession *botSession = new WorldSession(botAccountId, NULL, SEC_PLAYER, 1,0, LOCALE_enUS);
             botSession->m_Address = "bot";
             botSession->HandlePlayerLogin(lqh); // will delete lqh
             Player* bot = botSession->GetPlayer();
