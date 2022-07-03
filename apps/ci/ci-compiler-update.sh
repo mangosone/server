6cce1877365f1df3240cc9a3e2ab0e74f51eff09
Antz <billy1arm@users.noreply.github.com>
2022-07-03 22:21:04 +0100

    clean up file

--- apps/ci/ci-compiler-update.sh
+++ apps/ci/ci-compiler-update.sh
@@ -5,3 +5,4 @@ set -e
 # Update to Clang Compilers
 time sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
 time sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang 100
+
