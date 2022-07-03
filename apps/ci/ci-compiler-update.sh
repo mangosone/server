4a0f47b5fb33894f218ae2d52dca25ac290a09cf
Antz <billy1arm@users.noreply.github.com>
2022-07-03 22:25:01 +0100

    Copy file from M0

--- apps/ci/ci-compiler-update.sh
+++ apps/ci/ci-compiler-update.sh
@@ -5,4 +5,3 @@ set -e
 # Update to Clang Compilers
 time sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
 time sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang 100
-
