6208ebcd6dcb60ca0902780c6ebc23627632eb58
Antz <billy1arm@users.noreply.github.com>
2026-06-11 07:41:39 +0100

    Fix compile issues from previous commit

--- src/game/Object/ObjectMgr.h
+++ src/game/Object/ObjectMgr.h
@@ -499,8 +499,9 @@ class ObjectMgr
 
         typedef UNORDERED_MAP<uint32, PointOfInterest> PointOfInterestMap;
 
-
         typedef UNORDERED_MAP<uint32, PetCreateSpellEntry> PetCreateSpellMap;
+
+        typedef std::multimap<uint32 /*mapId*/, uint32 /*guid*/> ActiveCreatureGuidsOnMap;
 
         void LoadGameobjectInfo();
 
@@ -915,6 +916,11 @@ class ObjectMgr
                     break;
                 }
             }
+        }
+
+        ActiveCreatureGuidsOnMap const* GetActiveCreatureGuids() const
+        {
+            return &m_activeCreatures;
         }
 
         CreatureLocale const* GetCreatureLocale(uint32 entry) const
@@ -1377,8 +1383,6 @@ class ObjectMgr
         typedef std::map<uint32, std::vector<std::string> > HalfNameMap;
         HalfNameMap PetHalfName0;
         HalfNameMap PetHalfName1;
-
-        typedef std::multimap<uint32 /*mapId*/, uint32 /*guid*/> ActiveCreatureGuidsOnMap;
 
         // Array to store creature stats, Max creature level + 1 (for data alignement with in game level)
         CreatureClassLvlStats m_creatureClassLvlStats[DEFAULT_MAX_CREATURE_LEVEL + 1][MAX_CREATURE_CLASS][MAX_EXPANSION + 1];
