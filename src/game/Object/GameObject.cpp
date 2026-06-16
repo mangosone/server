4377e494b616b1d9d37e811df2b1d7cc3286b5c5
Bo Zimmerman <bo@zimmers.net>
2026-06-16 01:07:50 -0500

    fix for quest lootable items (#373)

--- src/game/Object/GameObject.cpp
+++ src/game/Object/GameObject.cpp
@@ -1266,11 +1266,15 @@ bool GameObject::ActivateToQuest(Player* pTarget) const
             {
                 // look for battlegroundAV for some objects which are only activated after mine gots captured by own team
                 if (GetEntry() == BG_AV_OBJECTID_MINE_N || GetEntry() == BG_AV_OBJECTID_MINE_S)
+                {
                     if (BattleGround* bg = pTarget->GetBattleGround())
+                    {
                         if (bg->GetTypeID() == BATTLEGROUND_AV && !(((BattleGroundAV*)bg)->PlayerCanDoMineQuest(GetEntry(), pTarget->GetTeam())))
                         {
                             return false;
                         }
+                    }
+                }
                 return true;
             }
             break;
