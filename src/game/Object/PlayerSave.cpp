45962441f17e0064e861f120650247669f1016d3
H0zen <wowtech@ub.ro>
2026-07-30 23:10:28 +0300

    Pets on trans VINI

--- src/game/Object/PlayerSave.cpp
+++ src/game/Object/PlayerSave.cpp
@@ -194,9 +194,12 @@ void Player::SaveToDB()
         Transport* vessel = m_transport;
         if (!vessel)
         {
-            if (TransportMap* hull = GetMap()->AsTransport())
+            if (Map* on = FindMap())
             {
-                vessel = hull->Vessel();
+                if (TransportMap* hull = on->AsTransport())
+                {
+                    vessel = hull->Vessel();
+                }
             }
         }
 
